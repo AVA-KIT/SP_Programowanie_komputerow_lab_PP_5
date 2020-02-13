51cfee064ce782af1eb39852d7b153b5b48c5686
AVA-KIT <gazda.edu@gmail.com>
2020-02-13 01:02:55 +0100

    Zadanie 9

--- Lab5.9.py
+++ Lab5.9.py
@@ -1,9 +1,17 @@
 from datetime import date
 
+year = input('Podaj rok: ')
+month = input('Podaj miesiąc: ')
+day = input('Podaj dzień: ')
 
+def daysToEndOfYear(year = date.today().year,month = date.today().month,day = date.today().day):
+    delta = date(year,12,31) - date(year,month,day)
+    print(delta.days)
 
-def daysToEndOfYear(y = date.today().year , m = date.today().month,d = date.today().day):
-    r = input('Podaj rok: ')
-    print(y,m,d)
-
-daysToEndOfYear(r)
+print('Data: 2020-1-20. Dni do końca roku:', end=" "), daysToEndOfYear(2020,1,20)
+print('Data: dzisiaj. Dni do końca roku:', end=" "), daysToEndOfYear()
+daysToEndOfYear(day = 23, month =12, year = 2023)
+daysToEndOfYear()
+daysToEndOfYear(year=2021)
+daysToEndOfYear(year=2021, month=10)
+daysToEndOfYear(day=1)
